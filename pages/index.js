c0d7f12f0ed5ca58fed32d8073342cf756dbadd0
Anders Askeland <anders.askeland@gmail.com>
2021-10-21 12:33:28 +0200

    Testing if github actions works

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <h1 className="text-2xl text-red-300">
-      Testing if github actions work on next.js
+      Testing if github actions work on next.js 
     </h1>
   )
 }
