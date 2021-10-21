85a05dd68660ff2531f1f737bb441fbd55df9d61
Anders Askeland <anders.askeland@gmail.com>
2021-10-21 12:53:49 +0200

    Changed back

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,8 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <><h1 className="text-2xl text-red-300">
-      Testing if github actions work on next.js.
-    </h1><h1>
-        This is test number two
-        Testing again!!!
-        Test!
-        teeeeeeest!
-      </h1>
+      Testing if github actions work on next.js.g
+    </h1>
       </>
   )
 }
