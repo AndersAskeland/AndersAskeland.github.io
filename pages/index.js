7655200c8df2e14c1d76df4a97a2b9bd650be974
Anders Askeland <anders.askeland@gmail.com>
2021-10-21 13:02:14 +0200

    Testing again

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <><h1 className="text-2xl text-red-300">
       Testing if github actions work on next.js.g
+      Testing again
     </h1>
       </>
   )
